e559d9c3923fd5543f900fc8451f9ca564c3f227
Ruslan Dunets <dunecruslan2@gmail.com>
2024-02-02 13:25:01 +0200

    added discover section and calendar

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,20 @@
 // import mobileNav from './modules/mobile-nav.js';
 // mobileNav();
 
-import autoComplete from '@tarekraafat/autocomplete.js'
-import countryList from './helpers/countryList'
+import autoCompleteFunc from './modules/autocomplete'
+autoCompleteFunc()
 
-const autoCompleteJS = new autoComplete({
-	selector: '#autoCompleteCountry',
-	placeHolder: 'e.g Bali, Indonesia',
-	data: {
-		src: countryList,
-		cache: true,
-	},
-	resultItem: {
-		highlight: true,
-	},
-	events: {
-		input: {
-			selection: event => {
-				const selection = event.detail.selection.value
-				autoCompleteJS.input.value = selection
-			},
-		},
-	},
+// Calendar
+import { easepick } from '@easepick/core'
+import { TimePlugin } from '@easepick/time-plugin'
+
+const picker = new easepick.create({
+	element: document.getElementById('datepicker'),
+	css: [
+		'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
+		'https://cdn.jsdelivr.net/npm/@easepick/time-plugin@1.2.1/dist/index.css',
+	],
+	// format: 'HH:mm, DD/MM/YY',
+	zIndex: 10,
+	plugins: [TimePlugin],
 })
