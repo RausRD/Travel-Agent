23f0974bcd6baf6b5653ee164d56c678d2366eb4
Ruslan Dunets <dunecruslan2@gmail.com>
2024-02-05 12:25:36 +0200

    added popular section and swiper

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,19 @@
 // mobileNav();
 
 import autoCompleteFunc from './modules/autocomplete'
+
 autoCompleteFunc()
 
 // Calendar
 import { easepick } from '@easepick/core'
 import { TimePlugin } from '@easepick/time-plugin'
 
+// Swiper
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle'
+
+// import styles bundle
+import 'swiper/css/bundle'
 const picker = new easepick.create({
 	element: document.getElementById('datepicker'),
 	css: [
@@ -17,4 +24,13 @@ const picker = new easepick.create({
 	// format: 'HH:mm, DD/MM/YY',
 	zIndex: 10,
 	plugins: [TimePlugin],
+})
+
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 4,
+	spaceBetween: 32,
+	navigation: {
+		nextEl: '#sliderNext',
+		prevEl: '#sliderPrev',
+	},
 })
