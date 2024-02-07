fb6ddeeecfc0179d5573b8ec1e0fef2ee1b849b8
Ruslan Dunets <dunecruslan2@gmail.com>
2024-02-07 13:06:04 +0200

    added other adaptive, finalize project

--- src/js/modules/swiper-testimonials.js
+++ src/js/modules/swiper-testimonials.js
@@ -2,48 +2,60 @@
 import Swiper from 'swiper/bundle'
 // import styles bundle
 import 'swiper/css/bundle'
-function swiperFunc() {
-	const swiper = new Swiper('#testimonials-col-1', {
-		direction: 'vertical',
-		slidesPerView: 2,
-		spaceBetween: 32,
-		grabCursor: true,
-		ally: false,
-		freeMode: true,
-		speed: 4000,
-		loop: true,
-		autoplay: {
-			delay: 0.0,
-			disableOnInteraction: false
-		}
-	})
-	const swiper2 = new Swiper('#testimonials-col-2', {
-		direction: 'vertical',
-		slidesPerView: 2,
-		spaceBetween: 32,
-		grabCursor: true,
-		ally: false,
-		freeMode: true,
-		speed: 4000,
-		loop: true,
-		autoplay: {
-			delay: 0.0,
-			disableOnInteraction: false
-		}
-	})
-	const swiper3 = new Swiper('#testimonials-col-3', {
-		direction: 'vertical',
-		slidesPerView: 2,
-		spaceBetween: 32,
-		grabCursor: true,
-		ally: false,
-		freeMode: true,
-		speed: 4000,
-		loop: true,
-		autoplay: {
-			delay: 0.0,
-			disableOnInteraction: false
-		}
-	})
+let swiper1, swiper2, swiper3, swiperHorizontal
+const configVertical = {
+	direction: 'vertical',
+	slidesPerView: 2,
+	spaceBetween: 0,
+	grabCursor: true,
+	ally: false,
+	freeMode: true,
+	speed: 4000,
+	loop: true,
+	autoplay: {
+		delay: 0.0,
+		disableOnInteraction: false,
+	},
 }
-export default swiperFunc
+const configHorizontal = {
+	direction: 'horizontal',
+	slidesPerView: 2,
+	spaceBetween: 32,
+	grabCursor: true,
+	ally: false,
+	freeMode: true,
+	speed: 4000,
+	loop: true,
+	autoplay: {
+		delay: 0.0,
+		disableOnInteraction: false,
+	},
+	breakpoints: {
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 40,
+		},
+	},
+}
+const breakpoint = window.matchMedia('(max-width:1023px)')
+const breackpointCheker = function () {
+	if (breakpoint.matches === true) {
+		swiperHorizontal = new Swiper(
+			'#testimonials-horizontal-swiper',
+			configHorizontal
+		)
+		if (swiper1 !== undefined) swiper1.destroy(true, true)
+		if (swiper2 !== undefined) swiper2.destroy(true, true)
+		if (swiper3 !== undefined) swiper3.destroy(true, true)
+		return
+	} else if (breakpoint.matches === false) {
+		swiper1 = new Swiper('#testimonials-col-1', configVertical)
+		swiper2 = new Swiper('#testimonials-col-2', configVertical)
+		swiper3 = new Swiper('#testimonials-col-3', configVertical)
+		if (swiperHorizontal !== undefined) swiperHorizontal.destroy(true, true)
+		return
+	}
+}
+breakpoint.addListener(breackpointCheker)
+breackpointCheker()
+// breakpoint.addListener(breackpointCheker)
